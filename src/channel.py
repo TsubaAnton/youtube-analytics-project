bb76ed3ae496fa47d918c6187a06615922e82b1e
TsubaAnton <antontsuba2003@gmail.com>
2023-12-14 23:40:20 +0200

    Homework1

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,5 @@
+import os
+from googleapiclient.discovery import build
 
 
 class Channel:
@@ -5,8 +7,32 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.api_key = os.environ.get("API_KEY")
+        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
+
+    def get_channel_info(self):
+        """Получает информацию о канале через API."""
+        request = self.youtube.channels().list(
+            part='snippet,contentDetails,statistics',
+            id=self.channel_id
+        )
+        response = request.execute()
+        return response.get('items', [])[0] if response.get('items') else None
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel_info = self.get_channel_info()
+
+        if channel_info:
+            snippet = channel_info['snippet']
+            statistics = channel_info['statistics']
+
+            print(f"Title: {snippet['title']}")
+            print(f"Description: {snippet['description']}")
+            print(f"Published At: {snippet['publishedAt']}")
+            print(f"View Count: {statistics['viewCount']}")
+            print(f"Subscriber Count: {statistics['subscriberCount']}")
+            print(f"Video Count: {statistics['videoCount']}")
+        else:
+            print("Unable to fetch channel information.")
